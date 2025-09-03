a1d4ca9bba1d803a51e5c001f5f91e2cfff86ac3
Jeff Hsu <tibame201020@gmail.com>
2025-09-03 09:13:16 +0800

    initial

---
+++ grpc-server/src/test/java/custom/tibame201020/grpc_server/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package custom.tibame201020.grpc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
